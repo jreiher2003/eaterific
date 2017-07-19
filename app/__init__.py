92aa6a61dfcd240475dbae9e1191be903ec25f26
Jeff Reiher <jreiher2003@yahoo.com>
2017-07-19 14:52:23 +0000

    Commit changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,14 @@
 import os 
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy 
-from flask_bcrypt import Bcrypt 
-from flask_login import LoginManager, current_user 
+# from flask_bcrypt import Bcrypt 
+from flask_login import LoginManager#, current_user 
 from flask_script import Manager
 
 app = Flask(__name__) 
 app.config.from_object(os.environ["APP_SETTINGS"])
 db = SQLAlchemy(app) 
-bcrypt = Bcrypt(app)
-lm = LoginManager(app)
+# bcrypt = Bcrypt(app)
 manager = Manager(app) 
 
 from app.users.views import users_blueprint 
@@ -17,9 +16,10 @@ app.register_blueprint(users_blueprint)
 from app.rest.views import rest_blueprint 
 app.register_blueprint(rest_blueprint) 
 
-from app.users.models import Users 
+from app.users.models import *
+lm = LoginManager(app)
 
 @lm.user_loader 
 def load_user(id):
-    return Users.query.get(int(id)) 
+    return UserProfile.query.get(int(id)) 
 
--- manage.py
+++ manage.py
@@ -3,8 +3,8 @@ import unittest
 from app import app, db
 from flask_script import Manager, Server 
 from flask_migrate import Migrate, MigrateCommand 
-from app.users.models import Users, UserRegister, SocialLogin, ProviderName, AsyncOperationStatus, AsyncOperation, TodoItem
-
+# from app.users.models import Users, UserRegister, SocialLogin, ProviderName, AsyncOperationStatus, AsyncOperation, TodoItem
+from app.users.models import *
 
 manager = Manager(app) 
 migrate = Migrate(app, db)
@@ -13,24 +13,26 @@ manager.add_command('db', MigrateCommand)
 
 @manager.command 
 def create_users():
-    Users.__table__.create(db.engine)
-    UserRegister.__table__.create(db.engine)
-    ProviderName.__table__.create(db.engine)
-    SocialLogin.__table__.create(db.engine)
+    UserProfile.__table__.create(db.engine)
+    # Users.__table__.create(db.engine)
+    # UserRegister.__table__.create(db.engine)
+    # ProviderName.__table__.create(db.engine)
+    # SocialLogin.__table__.create(db.engine)
     AsyncOperationStatus.__table__.create(db.engine)
     AsyncOperation.__table__.create(db.engine)
-    TodoItem.__table__.create(db.engine)
+    # TodoItem.__table__.create(db.engine)
     print "created all users tables" 
 
 @manager.command 
 def drop_users():
-    TodoItem.__table__.drop(db.engine)
+    # TodoItem.__table__.drop(db.engine)
     AsyncOperation.__table__.drop(db.engine)
     AsyncOperationStatus.__table__.drop(db.engine)
-    SocialLogin.__table__.drop(db.engine)
-    ProviderName.__table__.drop(db.engine)
-    UserRegister.__table__.drop(db.engine)
-    Users.__table__.drop(db.engine)
+    # SocialLogin.__table__.drop(db.engine)
+    # ProviderName.__table__.drop(db.engine)
+    # UserRegister.__table__.drop(db.engine)
+    # Users.__table__.drop(db.engine)
+    UserProfile.__table__.drop(db.engine)
     print "dropped all users tables" 
 
 @manager.command 
