2e44b775b4c380c6d0eda081d0f6d48b15f61b23
Jeff Reiher <jreiher2003@yahoo.com>
2018-02-10 19:44:11 +0000

    Update db with new models, got to joining Restaurant to Cusine and got stuck

--- app/rest/models.py
+++ app/rest/models.py
@@ -2,56 +2,49 @@ import os
 from app import db
 from slugify import slugify
 
+
 class State(db.Model):
     __tablename__ = "state"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     abbr = db.Column(db.String)
-    state_link = db.Column(db.String)
+    counties = db.relationship("County")
+    cities = db.relationship("City")
+    restaurants = db.relationship("Restaurant")
 
     @property 
     def url_slug_state(self):
         return slugify(self.name)
-
-class MetroAssoc(db.Model):
-    __tablename__ = "metro_assoc" 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    metro_link = db.Column(db.String) 
-    state_id = db.Column(db.Integer, db.ForeignKey("state.id"), index=True)
-    state = db.relationship("State", foreign_keys=[state_id])
-
+        
 class County(db.Model):
     __tablename__ = "county"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     state_id = db.Column(db.Integer, db.ForeignKey("state.id"), index=True)
-    state = db.relationship("State", foreign_keys=[state_id]) 
-
+    cities = db.ForeignKey("City")
+    restaurants = db.ForeignKey("Restaurant")
+     
     @property 
     def url_slug_county(self):
         return slugify(self.name)
-
-class CityMetro(db.Model):
-    __tablename__ = "city_metro" 
+        
+class City(db.Model):
+    __tablename__ = "city" 
     id = db.Column(db.Integer, primary_key=True)
     city_name = db.Column(db.String)
     neighborhood_name = db.Column(db.String)
-    city_link = db.Column(db.String)
     metro_area = db.Column(db.Boolean)
     r_total = db.Column(db.Integer)
     state_id = db.Column(db.Integer, db.ForeignKey("state.id"), index=True)
-    state = db.relationship("State", foreign_keys=[state_id])
     county_id = db.Column(db.Integer, db.ForeignKey("county.id"), index=True)
-    county = db.relationship("County", foreign_keys=[county_id]) 
-
+    restaurants = db.ForeignKey("Restaurant")
+    
     @property 
     def url_slug_city(self):
         return slugify(self.city_name)
-
-
-class RestaurantLinks(db.Model):
-    __tablename__ = "restaurant_links"
+        
+class Restaurant(db.Model):
+    __tablename__ = "restaurant"
     id = db.Column(db.Integer, primary_key=True)
     rest_name = db.Column(db.String)
     rest_link = db.Column(db.String)
@@ -80,22 +73,18 @@ class RestaurantLinks(db.Model):
     good_for_kids = db.Column(db.Boolean)
     menu_url_id = db.Column(db.String)
     menu_link_pdf = db.Column(db.String)
-    state_id = db.Column(db.Integer, db.ForeignKey("state.id"), index=True)
-    state = db.relationship("State", foreign_keys=state_id)
-    county_id = db.Column(db.Integer, db.ForeignKey("county.id"), index=True)
-    county = db.relationship("County", foreign_keys=county_id) 
-    city_metro_id = db.Column(db.Integer, db.ForeignKey("city_metro.id"), index=True)
-    city_metro = db.relationship("CityMetro", foreign_keys=city_metro_id)
-    cusine = db.relationship('Cusine', secondary='restaurant_links_cusine', backref=db.backref('restaurant_links', lazy='dynamic', cascade="all, delete-orphan", single_parent=True))
-    rlc = db.relationship('RestaurantLinksCusine', backref=db.backref('restaurant_links', cascade="all, delete-orphan", single_parent=True))
-    menu = db.relationship("Menu", uselist=False, backref="restaurant_links")
 
+    state_id = db.Column(db.Integer, db.ForeignKey("state.id"), index=True)
+    county_id = db.Column(db.Integer, db.ForeignKey("county.id"), index=True)
+    city_id = db.Column(db.Integer, db.ForeignKey("city.id"), index=True)
+    menu = db.relationship("Menu")
+    
     @property 
     def url_slug_rest(self):
         if self.neighborhood_name is not None:
             return slugify(self.neighborhood_name)
         return slugify(self.rest_name)
-
+        
 class Cusine(db.Model):
     __tablename__ = "cusine"
     id = db.Column(db.Integer, primary_key=True)
@@ -105,47 +94,68 @@ class Cusine(db.Model):
     def url_slug_cusine(self):
         return slugify(self.name)
 
-class RestaurantLinksCusine(db.Model):
-    __tablename__ = "restaurant_links_cusine"
+class RestaurantCusine(db.Model):
+    __tablename__ = "restaurant_cusine"
     id = db.Column(db.Integer, primary_key=True)
-    restaurant_links_id = db.Column(db.Integer, db.ForeignKey('restaurant_links.id', ondelete='CASCADE'), index=True)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id', ondelete='CASCADE'), index=True)
     cusine_id = db.Column(db.Integer, db.ForeignKey('cusine.id', ondelete='CASCADE'), index=True)
-    cusine = db.relationship('Cusine', backref=db.backref('restaurant_links_cusine', lazy='dynamic', cascade="all, delete-orphan"))
 
-    # state_id = db.Column(db.Integer, db.ForeignKey("state.id"), index=True)
-    # state = db.relationship("State", foreign_keys=state_id)
-    # county_id = db.Column(db.Integer, db.ForeignKey("county.id"), index=True)
-    # county = db.relationship("County", foreign_keys=county_id) 
-    # city_metro_id = db.Column(db.Integer, db.ForeignKey("city_metro.id"), index=True)
-    # city_metro = db.relationship("CityMetro", foreign_keys=city_metro_id)
+class RestaurantCoverImage(db.Model):
+    __tablename__ = "restaurant_cover_image"
+    id = db.Column(db.Integer, primary_key=True)
+    cover_photo = db.Column(db.String)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id', ondelete='CASCADE'), index=True)
+
+class RestaurantImages(db.Model):
+    __tablename__ = "restaurant_images"
+    id = db.Column(db.Integer, primary_key=True)
+    photos = db.Column(db.String)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id', ondelete='CASCADE'), index=True)
 
 class Menu(db.Model):
     __tablename__ = "menu"
     id = db.Column(db.Integer, primary_key=True) 
     name = db.Column(db.String)
-    restaurant_links_id = db.Column(db.Integer, db.ForeignKey('restaurant_links.id', ondelete='CASCADE'), index=True)
-    category = db.relationship('Category', secondary='restaurant_menu_category', order_by='RestaurantMenuCategory.id', backref=db.backref('menu', lazy='dynamic', cascade='all, delete-orphan', single_parent=True))
-    menu_item = db.relationship('RestaurantMenuCategoryItem', order_by='RestaurantMenuCategoryItem.id', backref=db.backref('menu', cascade='all, delete-orphan', single_parent=True))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id', ondelete='CASCADE'), index=True)
+    menu_section = db.relationship("Section")
 
-class Category(db.Model):
-    __tablename__ = "category"
+class Section(db.Model):
+    __tablename__ = "section"
     id = db.Column(db.Integer, primary_key=True) 
     name = db.Column(db.String)
     description = db.Column(db.String)
-
-class RestaurantMenuCategory(db.Model):
-    __tablename__ = "restaurant_menu_category"
-    id = db.Column(db.Integer, primary_key=True)
-    restaurant_links_id = db.Column(db.Integer, db.ForeignKey('restaurant_links.id', ondelete='CASCADE'), index=True)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id', ondelete='CASCADE'), index=True)
     menu_id = db.Column(db.Integer, db.ForeignKey('menu.id', ondelete='CASCADE'), index=True)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id', ondelete='CASCADE'), index=True)
-
-class RestaurantMenuCategoryItem(db.Model):
-    __tablename__ = "restaurant_menu_category_item"
+    items = db.relationship("MenuItem")
+ 
+class MenuItem(db.Model):
+    __tablename__ = "menu_item"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     description = db.Column(db.String)
-    price = db.Column(db.String)
-    restaurant_links_id = db.Column(db.Integer, db.ForeignKey('restaurant_links.id', ondelete='CASCADE'), index=True)
     menu_id = db.Column(db.Integer, db.ForeignKey('menu.id', ondelete='CASCADE'), index=True)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id', ondelete='CASCADE'), index=True)
+    section_id = db.Column(db.Integer, db.ForeignKey('section.id', ondelete='CASCADE'), index=True)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id', ondelete='CASCADE'), index=True)
+    price_items = db.relationship("ItemPrice")
+
+class ItemPrice(db.Model):
+    __tablename__ = "item_price"
+    id = db.Column(db.Integer, primary_key=True)
+    price_title = db.Column(db.String)
+    price_value = db.Column(db.String)
+    menu_id = db.Column(db.Integer, db.ForeignKey('menu.id', ondelete='CASCADE'), index=True)
+    section_id = db.Column(db.Integer, db.ForeignKey('section.id', ondelete='CASCADE'), index=True)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id', ondelete='CASCADE'), index=True)
+    menu_item_id = db.Column(db.Integer, db.ForeignKey('menu_item.id', ondelete='CASCADE'), index=True) 
+
+class ItemAddon(db.Model):
+    __tablename__ = "item_addon"
+    id = db.Column(db.Integer, primary_key=True)
+    addon_title = db.Column(db.String)
+    addon_value = db.Column(db.String)
+    menu_id = db.Column(db.Integer, db.ForeignKey('menu.id', ondelete='CASCADE'), index=True)
+    section_id = db.Column(db.Integer, db.ForeignKey('section.id', ondelete='CASCADE'), index=True)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id', ondelete='CASCADE'), index=True)
+    menu_item_id = db.Column(db.Integer, db.ForeignKey('menu_item.id', ondelete='CASCADE'), index=True) 
+
+
--- app/rest/views.py
+++ app/rest/views.py
@@ -17,36 +17,62 @@ def about():
 @app.route("/<path:url_slug_state>/<int:state_id>/")
 def state_page(url_slug_state,state_id):
     county = County.query.filter_by(state_id=state_id).all()
-    city_metro = CityMetro.query.filter_by(state_id=state_id, metro_area=False).order_by(desc(CityMetro.r_total)).all()
-    canada = CityMetro.query.filter(CityMetro.state_id==state_id, CityMetro.county_id==None).order_by(desc(CityMetro.r_total)).all()
+    city_metro = City.query.filter_by(state_id=state_id, metro_area=False).order_by(desc(City.r_total)).all()
+    canada = City.query.filter(City.state_id==state_id, City.county_id==None).order_by(desc(City.r_total)).all()
     state_name = State.query.filter_by(id=state_id).one()
     return render_template("state_page.html", url_slug_state=url_slug_state,state_name=state_name.name, state_id=state_id, county=county, city_metro=city_metro, canada=canada)
 
-@app.route("/<path:url_slug_state>/<int:state_id>/<path:url_slug_city>/<int:city_metro_id>/page/<int:page>")
-def city_page(url_slug_state,state_id,url_slug_city,city_metro_id, page=1):
-    rest = RestaurantLinks.query.filter_by(state_id=state_id, city_metro_id=city_metro_id).order_by(asc(RestaurantLinks.rest_name)).paginate(page, 40, False)
-    rc = RestaurantLinks.query.filter_by(state_id=state_id, city_metro_id=city_metro_id).all()
-    rest_cusine1 = []      
-    for r in rc:
-        for j in r.rlc:
-            pass
-        for x in r.cusine:
-            rest_cusine1.append([x.name, x.url_slug_cusine, x.id])
-    import itertools 
-    k = sorted(rest_cusine1)
-    rest_cusine = [k[i] for i in range(len(k)) if i == 0 or k[i] != k[i-1]]# removes dups  
-    return render_template('city_page.html', rest=rest, rest_cusine=rest_cusine, url_slug_state=url_slug_state, state_id=state_id, url_slug_city=url_slug_city, city_metro_id=city_metro_id)
+@app.route("/<path:url_slug_state>/<int:state_id>/<path:url_slug_city>/<int:city_id>/<int:page>")
+def city_page(url_slug_state,state_id,url_slug_city,city_id, page=1):
+    # rest = RestaurantLinks.query.filter_by(state_id=state_id, city_metro_id=city_metro_id).all()
+    rest = Restaurant.query.filter_by(state_id=state_id, city_id=city_id).order_by(asc(Restaurant.rest_name)).paginate(page, 40, False)
+    state_name = State.query.filter_by(id=state_id).one()
+    city_name = City.query.filter_by(id=city_id).one()
+    rest_cusine=None
+    # rest_cusine = db.session.query(
+    #     Cusine, Restaurant, RestaurantCusine)\
+    #     .join(Restaurant)\
+    #     .join(RestaurantCusine)\
+    #     .filter(Cusine.id==RestaurantCusine.cusine_id)\
+    #     .filter(Restaurant.id==RestaurantCusine.restaurant_id)\
+    #     .filter(Restaurant.city_id==city_id, Restaurant.state_id==state_id).all()
+    # for r in rest_cusine:
+    #     print r.name
+    # rc = Restaurant.query.filter_by(state_id=state_id, city_id=city_id).all()
+    # rest_cusine1 = []      
+    # for r in rc:
+    #     for j in r.rlc:
+    #         pass
+    #     for x in r.cusine:
+    #         rest_cusine1.append([x.name, x.url_slug_cusine, x.id]) 
+    # k = sorted(rest_cusine1)
+    # rest_cusine = [k[i] for i in range(len(k)) if i == 0 or k[i] != k[i-1]]# removes dups 
+    return render_template('city_page.html', 
+        url_slug_state=url_slug_state, 
+        url_slug_city=url_slug_city, 
+        state_name=state_name.name,
+        state_id=state_id, 
+        city_name=city_name.city_name, 
+        city_id=city_id, 
+        rest=rest, 
+        rest_cusine=rest_cusine, 
+        rest_total=city_name.r_total)
 
-@app.route("/r/<path:url_slug_state>/<int:state_id>/<path:url_slug_city>/<int:city_metro_id>/<path:url_slug_rest>/<path:rest_id>/")
-def rest_page_city(url_slug_state, state_id, url_slug_city, city_metro_id, url_slug_rest, rest_id):
-    rest = RestaurantLinks.query.filter_by(id=rest_id).one()
-    menu_name = Menu.query.filter_by(restaurant_links_id=rest_id).one_or_none()
-    cats = Menu.query.filter_by(restaurant_links_id=rest.id).join(RestaurantMenuCategory).join(Category).join(RestaurantMenuCategoryItem).filter(RestaurantMenuCategory.restaurant_links_id==rest.id and RestaurantMenuCategory.category_id==Category.id).all()
-    return render_template("rest_page.html", rest=rest, menu_name=menu_name, cats=cats)
+@app.route("/<path:url_slug_state>/<int:state_id>/<path:url_slug_city>/<int:city_id>/<path:url_slug_rest>/<path:rest_id>/")
+def rest_page_city(url_slug_state, state_id, url_slug_city, city_id, url_slug_rest, rest_id):
+    rest = Restaurant.query.filter_by(id=rest_id).one()
+    return render_template("rest_page.html", rest=rest)
 
-@app.route("/c/<path:url_slug_state>/<int:state_id>/<path:url_slug_city>/<int:city_metro_id>/<path:url_slug_cusine>/<path:cusine_id>/")
-def page_cusine_city(url_slug_state, state_id, url_slug_city, city_metro_id, url_slug_cusine, cusine_id):
-    rest_c = RestaurantLinks.query.filter_by(state_id=state_id, city_metro_id=city_metro_id).join(RestaurantLinksCusine).join(Cusine).filter(RestaurantLinksCusine.cusine_id == cusine_id).order_by(asc(RestaurantLinks.rest_name)).all()
-    cusine_name = Cusine.query.filter_by(id=cusine_id).one()
-    return render_template('page_cusine_city.html', rest_c=rest_c, url_slug_state=url_slug_state, state_id=state_id, url_slug_city=url_slug_city, city_metro_id=city_metro_id, url_slug_cusine=url_slug_cusine, cusine_id=cusine_id, cusine_name=cusine_name.name)
+# @app.route("/r/<path:url_slug_state>/<int:state_id>/<path:url_slug_city>/<int:city_metro_id>/<path:url_slug_rest>/<path:rest_id>/")
+# def rest_page_city(url_slug_state, state_id, url_slug_city, city_metro_id, url_slug_rest, rest_id):
+#     rest = RestaurantLinks.query.filter_by(id=rest_id).one()
+#     menu_name = Menu.query.filter_by(restaurant_links_id=rest_id).one_or_none()
+#     cats = Menu.query.filter_by(restaurant_links_id=rest.id).join(RestaurantMenuCategory).join(Category).join(RestaurantMenuCategoryItem).filter(RestaurantMenuCategory.restaurant_links_id==rest.id and RestaurantMenuCategory.category_id==Category.id).all()
+#     return render_template("rest_page.html", rest=rest, menu_name=menu_name, cats=cats)
+
+# @app.route("/c/<path:url_slug_state>/<int:state_id>/<path:url_slug_city>/<int:city_metro_id>/<path:url_slug_cusine>/<path:cusine_id>/")
+# def page_cusine_city(url_slug_state, state_id, url_slug_city, city_metro_id, url_slug_cusine, cusine_id):
+#     rest_c = RestaurantLinks.query.filter_by(state_id=state_id, city_metro_id=city_metro_id).join(RestaurantLinksCusine).join(Cusine).filter(RestaurantLinksCusine.cusine_id == cusine_id).order_by(asc(RestaurantLinks.rest_name)).all()
+#     cusine_name = Cusine.query.filter_by(id=cusine_id).one()
+#     return render_template('page_cusine_city.html', rest_c=rest_c, url_slug_state=url_slug_state, state_id=state_id, url_slug_city=url_slug_city, city_metro_id=city_metro_id, url_slug_cusine=url_slug_cusine, cusine_id=cusine_id, cusine_name=cusine_name.name)
 
